db2541f9dbd20b1d173665939212d4280f834418
ChristianGaertner <christiangaertner.film@googlemail.com>
2013-08-13 18:17:25 +0200

    More moving and chaning

---
+++ php/ChatterBotApi/ChatterBotFactory.php
@@ -0,0 +1,58 @@
+<?php namespace ChatterBotApi;
+
+/*
+ * ChatterBotAPI
+ * Copyright (C) 2011 pierredavidbelanger@gmail.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Exception;
+
+/** 
+ * The ChatterBotFactory
+ */
+class ChatterBotFactory
+{
+	/**
+	 * Create a new a ChatterBot
+	 * @param  int    $type The ChatterBotType (hint: use the ChatterBotType constants)
+	 * @param  string $arg  BotId (only needed when using a PandoraBot)
+	 *
+	 * @return \ChatterBotApi\ChatterBot       The new Bot instance
+	 *
+	 * @throws \Exception When type not recognized or no BotId bassed to the PandoraBot
+	 */
+	public function create($type, $arg = null)
+	{
+		switch ($type) {
+			case ChatterBotType::CLEVERBOT:
+				return new _Cleverbot('http://www.cleverbot.com/webservicemin');
+			
+
+			case ChatterBotType::JABBERWACKY:
+				return new _Cleverbot('http://jabberwacky.com/webservicemin');
+			
+			case ChatterBotType::PANDORABOTS:
+				if ($arg == null) {
+					throw new Exception('PANDORABOTS needs a botid arg');
+				}
+			return new _Pandorabots($arg);
+
+			default:
+				throw new Exception('Type not recognized');
+				
+		}
+	}
+}
---
+++ php/ChatterBotApi/Implementation/CleverBot/Session.php
@@ -0,0 +1,95 @@
+<?php namespace ChatterBotApi\Implementation\CleverBot;
+
+/*
+ * ChatterBotAPI
+ * Copyright (C) 2011 pierredavidbelanger@gmail.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use ChatterBotApi;
+
+class Session extends ChatterBotSession
+{
+	/**
+	 * The assoc. bot of this session
+	 * @var [type]
+	 */
+	private $bot;
+
+	/**
+	 * Varibales used for creating the request
+	 * @var array
+	 */
+	private $vars;
+
+	/**
+	 * Constructor.
+	 * @param \ChatterBotApi\Implementation\CleverBot\CleverBot $bot The bot
+	 */
+	public function __construct($bot)
+	{
+		$this->bot = $bot;
+		$this->vars = array();
+		$this->vars['start'] = 'y';
+		$this->vars['icognoid'] = 'wsf';
+		$this->vars['fno'] = '0';
+		$this->vars['sub'] = 'Say';
+		$this->vars['islearning'] = '1';
+		$this->vars['cleanslate'] = 'false';
+	}
+
+	/**
+	 * Return new thought based on given thought
+	 * @param  \ChatterBotApi\ChatterBotTought $thought The previous thought
+	 * @return \ChatterBotApi\ChatterBotTought          The new thought.
+	 */
+	public function thinkThought($thought)
+	{
+		$this->vars['stimulus'] = $thought->getText();
+		$data = http_build_query($this->vars);
+		$dataToDigest = substr($data, 9, 20);
+		$dataDigest = md5($dataToDigest);
+		$this->vars['icognocheck'] = $dataDigest;
+		$response = _utils_post($this->bot->getUrl(), $this->vars);
+		$responseValues = split("\r", $response);
+		//self.vars['??'] = _utils_string_at_index($responseValues, 0);
+		$this->vars['sessionid'] = _utils_string_at_index($responseValues, 1);
+		$this->vars['logurl'] = _utils_string_at_index($responseValues, 2);
+		$this->vars['vText8'] = _utils_string_at_index($responseValues, 3);
+		$this->vars['vText7'] = _utils_string_at_index($responseValues, 4);
+		$this->vars['vText6'] = _utils_string_at_index($responseValues, 5);
+		$this->vars['vText5'] = _utils_string_at_index($responseValues, 6);
+		$this->vars['vText4'] = _utils_string_at_index($responseValues, 7);
+		$this->vars['vText3'] = _utils_string_at_index($responseValues, 8);
+		$this->vars['vText2'] = _utils_string_at_index($responseValues, 9);
+		$this->vars['prevref'] = _utils_string_at_index($responseValues, 10);
+		//$this->vars['??'] = _utils_string_at_index($responseValues, 11);
+		$this->vars['emotionalhistory'] = _utils_string_at_index($responseValues, 12);
+		$this->vars['ttsLocMP3'] = _utils_string_at_index($responseValues, 13);
+		$this->vars['ttsLocTXT'] = _utils_string_at_index($responseValues, 14);
+		$this->vars['ttsLocTXT3'] = _utils_string_at_index($responseValues, 15);
+		$this->vars['ttsText'] = _utils_string_at_index($responseValues, 16);
+		$this->vars['lineRef'] = _utils_string_at_index($responseValues, 17);
+		$this->vars['lineURL'] = _utils_string_at_index($responseValues, 18);
+		$this->vars['linePOST'] = _utils_string_at_index($responseValues, 19);
+		$this->vars['lineChoices'] = _utils_string_at_index($responseValues, 20);
+		$this->vars['lineChoicesAbbrev'] = _utils_string_at_index($responseValues, 21);
+		$this->vars['typingData'] = _utils_string_at_index($responseValues, 22);
+		$this->vars['divert'] = _utils_string_at_index($responseValues, 23);
+		$responseThought = new ChatterBotThought();
+		$responseThought->setText(_utils_string_at_index($responseValues, 16));
+		return $responseThought;
+	}
+}
