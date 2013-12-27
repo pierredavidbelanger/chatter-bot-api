60e5d9e4dedaf2e65cb09fb06f58cb18088f23d2
Arnaud Aliès <mouu@hush.com>
2013-12-27 17:33:30 +0100

    md5 module is deprecated

--- python/chatterbotapi.py
+++ python/chatterbotapi.py
@@ -1,4 +1,4 @@
-import md5
+import hashlib
 import urllib
 import urllib2
 import uuid
@@ -93,7 +93,7 @@ class _CleverbotSession(ChatterBotSession):
         self.vars['stimulus'] = thought.text
         data = urllib.urlencode(self.vars)
         data_to_digest = data[9:self.bot.endIndex]
-        data_digest = md5.new(data_to_digest).hexdigest()
+        data_digest = hashlib.md5(data_to_digest).hexdigest()
         data = data + '&icognocheck=' + data_digest
         url_response = urllib2.urlopen(self.bot.url, data)
         response = url_response.read()
