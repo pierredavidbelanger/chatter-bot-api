ae643c8b716f168d648f305022500a5540da69ac
Ryan <cloherty.ryan@gmail.com>
2016-11-15 17:33:55 -0500

    Set leftoverLoops to zero if <1 to keep while running infinitely
    Also added comments and stuff

--- java/src/main/java/com/google/code/chatterbotapi/ChatBotMeetsChatbot.java
+++ java/src/main/java/com/google/code/chatterbotapi/ChatBotMeetsChatbot.java
@@ -23,14 +23,14 @@ import java.util.Scanner;
 public class ChatBotMeetsChatbot
 {
     public static void main(String[] args) throws Exception
-	{
+    {
         String s = "Hi";
         String pandorabotBotID = "b0dafd24ee35a477";
         int botChoiceOne = 1;
         int botChoiceTwo = 1;
         int conversationLoopNum = 0;
-		int leftoverLoops = 0;
-		boolean shouldStillRun = true;
+        int leftoverLoops = 0;
+        boolean shouldStillRun = true;
         String botOneType = "CLEVERBOT";
         String botTwoType = "CLEVERBOT";
         Scanner input = new Scanner(System.in);
@@ -50,7 +50,7 @@ public class ChatBotMeetsChatbot
         System.out.print("How many times will you like to cycle through the exchange of words between the bots?\nCan be any integer (<1 is infinite and will require CTR + C)\n>");
         conversationLoopNum = input.nextInt();
         
-        switch(botChoiceOne)
+        switch(botChoiceOne)    //The first bot choice will be converted into a string later to be converted to enum value
         {
             case 1: botOneType = "CLEVERBOT";
             break;
@@ -60,7 +60,7 @@ public class ChatBotMeetsChatbot
             break;
         }
 
-        switch(botChoiceTwo)
+        switch(botChoiceTwo)    //The second bot choice will be converted into a string later to be converted to enum value
         {
             case 1: botTwoType = "CLEVERBOT";
             break;
@@ -71,37 +71,43 @@ public class ChatBotMeetsChatbot
         }
         
         
-        ChatterBot bot1 = factory.create(ChatterBotType.valueOf(botOneType), pandorabotBotID);
+        ChatterBot bot1 = factory.create(ChatterBotType.valueOf(botOneType), pandorabotBotID);  //botOneType will be converted to proper enum value (found in ChatterBotType.java)
         ChatterBotSession bot1session = bot1.createSession();
 
-        ChatterBot bot2 = factory.create(ChatterBotType.valueOf(botTwoType), pandorabotBotID);
+        ChatterBot bot2 = factory.create(ChatterBotType.valueOf(botTwoType), pandorabotBotID);  //botTwoType will be converted to proper enum value (found in ChatterBotType.java)
         ChatterBotSession bot2session = bot2.createSession();
         
-		if (conversationLoopNum < 1)
-		{
-			shouldStillRun = true;
-		}
-		else
-		{
-			leftoverLoops = conversationLoopNum;
-			shouldStillRun = true;
-		}
-		
+        /*
+         * This would be so much simpler if I had no option for infinite running.
+         * This if/else will set up the number to be decreased in the while loop below.
+         */
+        
+        if (conversationLoopNum < 1)
+        {
+            shouldStillRun = true;
+            leftoverLoops = 0;
+        }
+        else
+        {
+            leftoverLoops = conversationLoopNum;
+            shouldStillRun = true;
+        }
+        
         
         while (shouldStillRun)
-		{
-			if (leftoverLoops == 1)
-			{
-				shouldStillRun = false;
-			}
-			
+        {
+            if (leftoverLoops == 1) //This is to catch when the loops run out but will not if the value is already zero
+            {
+                shouldStillRun = false;
+            }
+            
             System.out.println("bot1> " + s);
 
             s = bot2session.think(s);
             System.out.println("bot2> " + s);
 
             s = bot1session.think(s);
-			leftoverLoops--;
+            leftoverLoops--;    //Decrement the leftoverLoops value
         }
     }
 }
